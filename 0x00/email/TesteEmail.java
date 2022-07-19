695d543fa73fec52c0e15e349f4047c5f6f8cd02
mdelagracia <mdelagracia@ciandt.com>
2022-07-19 17:08:30 -0300

    Teste

---
+++ 0x00/email/TesteEmail.java
@@ -0,0 +1,25 @@
+public class TesteEmail {
+    public static boolean testar_email_com_arroba() {
+        if (Pessoa.emailValid("email_teste@dominio.com.br")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean testar_email_sem_arroba() {
+        if (Pessoa.emailValid("email_testedominio.com.br")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static boolean testar_email_mais_50_caracteres() {
+        if (Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
